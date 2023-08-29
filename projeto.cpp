de712afd478d176a0e62bc937f38ad3f50a8a5b9
Victor Reis Carlota <128194246+victorreiscarlota@users.noreply.github.com>
2023-08-29 14:06:01 -0300

    Update projeto.cpp

--- projeto.cpp
+++ projeto.cpp
@@ -1,108 +1,145 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <chrono>
 
-class Equipamento
-{
-public:
-    Equipamento(std::string codigo, std::string descricao) : codigo(codigo), descricao(descricao) {}
-
+class Equipamento {
+private:
     std::string codigo;
     std::string descricao;
-};
+    double valorDiario;
 
-class Aluguel
-{
 public:
-    Aluguel(class Cliente *cliente, class Equipamento *equipamento, double valor_diario, std::chrono::system_clock::time_point data_inicio, std::chrono::system_clock::time_point data_termino)
-        : cliente(cliente), equipamento(equipamento), valor_diario(valor_diario), data_inicio(data_inicio), data_termino(data_termino) {}
+    Equipamento(const std::string& codigo, const std::string& descricao, double valorDiario)
+        : codigo(codigo), descricao(descricao), valorDiario(valorDiario) {}
 
-    double valor_total()
-    {
-        std::chrono::duration<double> duration = data_termino - data_inicio;
-        int dias = std::chrono::duration_cast<std::chrono::hours>(duration).count() / 24;
-        return valor_diario * dias;
+    const std::string& getCodigo() const {
+        return codigo;
     }
 
-    class Cliente *cliente;
-    class Equipamento *equipamento;
-    double valor_diario;
-    std::chrono::system_clock::time_point data_inicio;
-    std::chrono::system_clock::time_point data_termino;
+    const std::string& getDescricao() const {
+        return descricao;
+    }
+
+    double getValorDiario() const {
+        return valorDiario;
+    }
 };
 
-class Cliente
-{
-public:
-    Cliente(std::string nome) : nome(nome) {}
-
+class Cliente {
+private:
     std::string nome;
+
+public:
+    Cliente(const std::string& nome)
+        : nome(nome) {}
+
+    const std::string& getNome() const {
+        return nome;
+    }
 };
 
-int main()
-{
-    // Criação de alguns equipamentos
-    Equipamento equipamento1("E001", "Furadeira");
-    Equipamento equipamento2("E002", "Martelo");
+class Aluguel {
+private:
+    Cliente cliente;
+    Equipamento equipamento;
+    std::string dataInicio;
+    std::string dataTermino;
+    double valorTotal;
 
-    // Criação de um cliente
-    Cliente cliente1("João");
+public:
+    Aluguel(const Cliente& cliente, const Equipamento& equipamento, const std::string& dataInicio, const std::string& dataTermino)
+        : cliente(cliente), equipamento(equipamento), dataInicio(dataInicio), dataTermino(dataTermino) {
+        double numDias = 1.0; // Para Calcular o número de dias de aluguel
+        valorTotal = numDias * equipamento.getValorDiario();
+    }
 
-    // Criação de um novo aluguel
-    auto data_inicio = std::chrono::system_clock::now();
-    auto data_termino = data_inicio + std::chrono::hours(24 * 103); // 103 dias
-    Aluguel novo_aluguel(&cliente1, &equipamento1, 50.00, data_inicio, data_termino);
+    const Cliente& getCliente() const {
+        return cliente;
+    }
 
-    // Cálculo do valor total do aluguel
-    double valor_total = novo_aluguel.valor_total();
-    std::cout << "Valor total do aluguel: R$" << valor_total << std::endl;
+    const Equipamento& getEquipamento() const {
+        return equipamento;
+    }
 
-    // Armazenamento dos aluguéis
-    std::vector<Aluguel> alugueis_passados;
+    const std::string& getDataInicio() const {
+        return dataInicio;
+    }
 
-    // Adição do aluguel à lista de aluguéis passados
-    alugueis_passados.push_back(novo_aluguel);
+    const std::string& getDataTermino() const {
+        return dataTermino;
+    }
 
-    // Consulta de aluguéis por cliente
-    auto consulta_cliente = [&alugueis_passados](std::string nome_cliente)
-    {
-        for (const auto &aluguel : alugueis_passados)
-        {
-            if (aluguel.cliente->nome == nome_cliente)
-            {
-                std::cout << "Cliente: " << aluguel.cliente->nome
-                        << ", Equipamento: " << aluguel.equipamento->descricao
-                        << ", Valor: R$" << aluguel.valor_diario << " por dia" << std::endl;
+    double getValorTotal() const {
+        return valorTotal;
+    }
+};
+
+class GerenciadorAlugueis {
+private:
+    std::vector<Aluguel> alugueis;
+
+public:
+    void registrarAluguel(const Cliente& cliente, const Equipamento& equipamento, const std::string& dataInicio, const std::string& dataTermino) {
+        Aluguel novoAluguel(cliente, equipamento, dataInicio, dataTermino);
+        alugueis.push_back(novoAluguel);
+    }
+
+    double calcularFaturamentoMensal(int mes, int ano) const {
+        double faturamentoTotal = 0.0;
+        for (const Aluguel& aluguel : alugueis) {
+            // Verificação do mês e ano especificados do aluguel
+            // e somar o valor ao faturamentoTotal
+        }
+        return faturamentoTotal;
+    }
+
+    std::vector<Aluguel> consultarAlugueisCliente(const Cliente& cliente) const {
+        std::vector<Aluguel> alugueisCliente;
+        for (const Aluguel& aluguel : alugueis) {
+            if (aluguel.getCliente().getNome() == cliente.getNome()) {
+                alugueisCliente.push_back(aluguel);
             }
         }
-    };
+        return alugueisCliente;
+    }
+};
 
-    // Chamada da função de consulta
-    consulta_cliente("João");
+// Função que consulta os alugueis de um cliente especifico 
+void consultaCliente(const GerenciadorAlugueis& gerenciador, const Cliente& cliente) {
+    std::vector<Aluguel> alugueisCliente = gerenciador.consultarAlugueisCliente(cliente);
 
-    // Relatório mensal
-    auto relatorio_mensal = [&alugueis_passados](int mes, int ano)
-    {
-        double faturamento_total = 0.0;
-        for (const auto &aluguel : alugueis_passados)
-        {
-            std::time_t start_time = std::chrono::system_clock::to_time_t(aluguel.data_inicio);
-            struct std::tm *timeinfo = std::localtime(&start_time);
-            int aluguel_mes = timeinfo->tm_mon + 1; // tm_mon is 0-indexed
-            int aluguel_ano = timeinfo->tm_year + 1900;
+    std::cout << "Aluguéis do cliente " << cliente.getNome() << ":" << std::endl;
+    for (const Aluguel& aluguel : alugueisCliente) {
+        std::cout << "Equipamento: " << aluguel.getEquipamento().getDescricao()
+                << " | Data Início: " << aluguel.getDataInicio()
+                << " | Data Término: " << aluguel.getDataTermino()
+                << " | Valor Total: " << aluguel.getValorTotal() << std::endl;
+    }
+}
 
-            if (aluguel_mes == mes && aluguel_ano == ano)
-            {
-                faturamento_total += aluguel.valor_total();
-            }
-        }
-        std::cout << "Relatório do mês " << mes << "/" << ano << std::endl;
-        std::cout << "Faturamento total: R$" << faturamento_total << std::endl;
-    };
+// metodo que gera relatório mensal
+void relatorioMensal(const GerenciadorAlugueis& gerenciador, int mes, int ano) {
+    double faturamentoMensal = gerenciador.calcularFaturamentoMensal(mes, ano);
 
-    // Chamada do relatório mensal
-    relatorio_mensal(6, 2023);
+    std::cout << "Relatório Mensal - " << mes << "/" << ano << ":" << std::endl;
+    std::cout << "Faturamento Total: " << faturamentoMensal << std::endl;
+}
+
+int main() {
+    Equipamento equipamento1("E1", "Escavadeira", 200.0);
+    Equipamento equipamento2("E2", "Betoneira", 150.0);
+
+    Cliente cliente1("Cliente A");
+    Cliente cliente2("Cliente B");
+
+    GerenciadorAlugueis gerenciador;
+
+    gerenciador.registrarAluguel(cliente1, equipamento1, "2023-08-01", "2023-08-05");
+    gerenciador.registrarAluguel(cliente2, equipamento2, "2023-08-03", "2023-08-07");
+
+    consultaCliente(gerenciador, cliente1);
+    consultaCliente(gerenciador, cliente2);
+
+    relatorioMensal(gerenciador, 8, 2023);
 
     return 0;
 }
